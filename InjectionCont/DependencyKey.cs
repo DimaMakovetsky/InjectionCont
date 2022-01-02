e3e19279f7e504d4ce4a5c8b25b9d5c1001d9fb9
DimaMakovetsky <mister.chuppa@mail.ru>
2022-01-02 23:28:05 +0300

    Depen key

---
+++ InjectionCont/DependencyKey.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InjectionCont
+{
+    [AttributeUsage(AttributeTargets.Parameter)]
+    public class DependencyKey : Attribute
+    {
+        public ImplementationNumber ImplNumber { get; }
+
+        public DependencyKey(ImplementationNumber number)
+        {
+            this.ImplNumber = number;
+        }
+    }
+}
